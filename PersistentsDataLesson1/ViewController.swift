aedd72837530a480ac01e5625ff36010cc7cd95d
Ildar Zalyalov <Zalyalov.Ild@gmail.com>
2017-11-13 16:48:00 +0300

    lesson commit

--- PersistentsDataLesson1/ViewController.swift
+++ PersistentsDataLesson1/ViewController.swift
@@ -12,14 +12,45 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let currentUser = User(name: "Max", age: 18)
+        let currentUser2 = User(name: "Max", age: 18)
+        let currentUser3 = User(name: "Max", age: 18)
+        
+        let archiver = NSKeyedArchiver.archivedData(withRootObject: [currentUser, currentUser2, currentUser3])
+        
+        UserDefaults.standard.set(archiver, forKey: "User")
+        UserDefaults.standard.synchronize()
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if let currentUserData = UserDefaults.standard.data(forKey: "User") {
+            
+            guard let currentUsers = NSKeyedUnarchiver.unarchiveObject(with: currentUserData) as? [User] else { return }
+            
+            for model in currentUsers {
+                print("User.name: \(model.name) \nAge: \(model.age)")
+            }
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func readData() {
+        
+        if let dataFile = Bundle.main.path(forResource: "data", ofType: "plist") {
+            
+            if let dataDict = NSDictionary(contentsOfFile: dataFile) {
+                let models = dataDict["New item"] as? [[String : Any]]
+                
+            }
+            
+        }
+    }
 
 }
-
